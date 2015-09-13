ece406bf419a08f9793b0020cb72420dbf5e8b30
Jorundur <jorundurj@gmail.com>
2015-09-13 12:34:09 +0000

    making it so that we can have multiple rectangles of various sizes and let the ball bounce off of them

--- core/src/com/ru/tgra/assignment2/RectangleGraphic.java
+++ core/src/com/ru/tgra/assignment2/RectangleGraphic.java
@@ -6,23 +6,34 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.utils.BufferUtils;
 
 public class RectangleGraphic {
-	private static FloatBuffer vertexBuffer;
-	private static int vertexPointer;
+	//private static FloatBuffer vertexBuffer;
+	//private static int vertexPointer;
+	private FloatBuffer vertexBuffer;
+	private int vertexPointer;
 	
-	public RectangleGraphic()
+	private float x1, y1, x2, y2;
+	
+	public RectangleGraphic(float x1, float y1, float x2, float y2, int ptr)
 	{
-		//RectangleGraphic.vertexPointer = vertexPointer;
+		this.x1 = x1;
+		this.y1 = y1;
+		this.x2 = x2;
+		this.y2 = y2;
+		this.vertexPointer = ptr;
+		
+		float halfSide = Math.abs(y2-y1) / 2.0f;
 		
 		//VERTEX ARRAY IS FILLED HERE
-		float[] array = {-50.0f, 50.0f,
-		-50.0f, -50.0f,
-		50.0f, -50.0f,
-		50.0f, 50.0f};
+		float[] array = {-halfSide, halfSide,
+						 -halfSide, -halfSide,
+						 halfSide, -halfSide,
+						 halfSide, halfSide};
 		vertexBuffer = BufferUtils.newFloatBuffer(8);
 		vertexBuffer.put(array);
 		vertexBuffer.rewind();
 	}
 	
+	/*
 	public static void create(int vertexPointer) {
 		RectangleGraphic.vertexPointer = vertexPointer;
 		
@@ -35,9 +46,29 @@ public class RectangleGraphic {
 		vertexBuffer.put(array);
 		vertexBuffer.rewind();
 	}
-	public static void drawSolidSquare() {
-		Gdx.gl.glVertexAttribPointer(vertexPointer, 2, GL20.GL_FLOAT,
-				false, 0, vertexBuffer);
+	*/
+
+	public void drawSolidSquare() {
+		Gdx.gl.glVertexAttribPointer(this.vertexPointer, 2, GL20.GL_FLOAT,
+				false, 0, this.vertexBuffer);
 		Gdx.gl.glDrawArrays(GL20.GL_TRIANGLE_FAN, 0, 4);
 	}
+	
+	public float getX1()
+	{
+		return x1;
+	}
+	public float getY1()
+	{
+		return y1;
+	}
+	public float getX2()
+	{
+		return x2;
+	}
+	public float getY2()
+	{
+		return y2;
+	}
+	
 }
